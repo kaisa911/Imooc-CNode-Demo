7e5b60339aeadc54306cb603ec4d2789c9a0edd0
bufan.li <lbf911@126.com>
2019-01-15 19:26:17 +0800

    add views

--- client/views/topicDetail/index.jsx
+++ client/views/topicDetail/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+/* eslint react/prefer-stateless-function: 0 */
 class TopicDetail extends React.Component {
   render() {
     return <div>this TopicDetail</div>;
--- client/views/topicList/index.jsx
+++ client/views/topicList/index.jsx
@@ -1,5 +1,7 @@
+// eslint disabled
 import React from 'react';
 
+/* eslint react/prefer-stateless-function: 0 */
 class TopicList extends React.Component {
   render() {
     return <div>this TopicList</div>;
